29b17f665e9c663ffd24ffe1c9a7c8d1d67e995e
MewowKio <111491067+MewowKio@users.noreply.github.com>
2022-10-30 23:00:00 +1100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+var vg_1 = "worldHng.vg.json";
+vegaEmbed("#g1", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "Stacked.vg.json";
+vegaEmbed("#g2", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "linechart.vg.json";
+vegaEmbed("#g3", vg_3).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "Scatter.vg.json";
+vegaEmbed("#g4", vg_4).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "Symbomap.vg.json";
+vegaEmbed("#g5", vg_5).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
